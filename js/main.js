f00204f375436e673f96ac8570ef154f0962334f
Sergey Primirenkov <serg_miren@mail.ru>
2018-08-07 19:08:13 +0200

    update files

--- js/main.js
+++ js/main.js
@@ -9,11 +9,9 @@ navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("main-nav--closed")) {
     navMain.classList.remove("main-nav--closed");
     navMain.classList.add("main-nav--opened");
-    popupWrapper.style.display = 'block';
   } else {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
-    popupWrapper.style.display = 'none';
   }
 });
 
